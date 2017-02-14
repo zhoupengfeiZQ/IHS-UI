f59aa0c1e12d697a16e4c2ca0087b65324e56d86
zhoupengfei <pengfei.zhou@hand-china.com>
2017-02-14 19:01:25 +0800

    depart CURD

--- login/js/loginCtr.js
+++ login/js/loginCtr.js
@@ -1,7 +1,10 @@
 
 	var App = angular.module('myApp',['ui.router','ngResource',]);
-	App.controller('loginCtr',function($scope,$state,$stateParams,$filter,$interval){
-	
+	App.controller('loginCtr',function($scope,$state,$stateParams,$filter,$interval,$http){
+	$scope.visible=false;
+	$scope.switch=function(){
+		$scope.visible=false;
+	};
     function fadeIn(e) {
         e.className = "bg fadein"
     }
@@ -54,8 +57,27 @@
         }
     }, 6000);
 /*    });*/
-	
 
+	//登陆验证
+	$scope.login=function(){
+		$http.get("http://localhost:9090/ihs/login?id="+$scope.id+"&password="+$scope.password)
+  			.success(function (data) {
+  				if(data.result=="success1"){
+  					location.replace("../../system/pages/superBar.html");
+  				}else if (data.result=="success2") {
+  					location.replace("../../system/pages/docBar.html");
+  				}else if (data.result=="success3") {
+  					location.replace("../../system/pages/visBar.html");
+  				}else{
+  					$scope.prompt="账号或密码有误！";
+  					$scope.visible=true;
+  				}
+  			}).error(function (data) {
+  				$scope.prompt="登陆失败！";
+  				$scope.visible=true;
+  			});
+	}
+	
 });
 
 
