b5c50b231034f173c5203187bf4ea8b3669378e4
zhoupengfei <pengfei.zhou@hand-china.com>
2017-04-16 21:27:11 +0800

    ihs update current web files

---
+++ js/leftnav.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+		$("#main-nav li .nav-top-item").hover(
+			function () {
+				$(this).stop().animate({ paddingRight: "25px" }, 200);
+			}, 
+			function () {
+				$(this).stop().animate({ paddingRight: "15px" });
+			}
+		);
+
+});
+  
